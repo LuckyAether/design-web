86ee12dbcc450cc1f30950aca7f9c13dee059d95
Francisco Silvestre <francisco.silvestre@escolar.ifrn.edu.br>
2026-08-20 13:29:39 -0300

    Comcertando JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 alert("Olá mundo!");
 
- var nasc = 2008;
- let nome = "Francisco";
- const viva = true;
- let altura = 1.76;
+var nasc = 2008;
+let nome = "Francisco";
+const viva = true;
+let altura = 1.76;
 
- if (viva){
-    let saudacao = "Olá " + nome + "!";
-    let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
-    alert(saudacao+"\n"+msg);
- }else{
+if (viva){
+   let saudacao = "Olá "+ nome +"!";
+   let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
+   alert(saudacao+"\n"+msg);
+}else{
     alert("R.I.P");
- }
+}
