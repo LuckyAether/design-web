d74998623af6b2dbc2a688e2909fa9a0ea7305f6
Francisco Silvestre <francisco.silvestre@escolar.ifrn.edu.br>
2026-08-21 14:15:47 -0300

    Index e js atualizados

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,16 @@
-alert("Olá mundo!");
-
 var nasc = 2008;
-let nome = "'Francisco'";
+let nome = "Francisco";
 const viva = true;
-let altura = 1.76;
 
-if (viva){
-   let saudacao = "Olá "+ nome +"!";
-   let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
-   alert(saudacao+"\n"+msg);
-}else{
-    alert("R.I.P");
+function calcIdade(ano=2026){
+    let idade = ano - nasc;
+    alert(`Dentro de Função - Idade ${idade}`);
+    return idade;
 }
+
+calcIdade()
+/*
+alert(`Fora da funcao : Idade ${idade}`); 
+Erro pois a variavel let nao existe fora do escopo dela
+*/
+alert(`Fora da funcao : chamado calcIdade ${calcIdade(2027)}`);
