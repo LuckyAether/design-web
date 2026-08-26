382e9e27fac5f62d3910aa28004c57328c279691
Francisco Silvestre <francisco.silvestre@escolar.ifrn.edu.br>
2026-08-26 17:14:21 -0300

    adicionou: pode beber ou não

--- scripts/app.js
+++ scripts/app.js
@@ -7,10 +7,12 @@ function calcIdade(ano=2026){
     let menor;
     if (idade < 18){
         menor = true;
+        var podebeber = false;
     }else{
         menor = false;
+        var podebeber = true;
     }
-    alert(`${nome} é menor de idade? ${menor}\nIdade:{idade}`)
+    alert(`${nome} é menor de idade? ${menor}\nIdade: ${idade}\nPode beber:${podebeber}`)
     return idade;
 }
 
