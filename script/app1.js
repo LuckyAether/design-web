986a565e3cf9023b2a15e3bf00b72ecf85dff168
Francisco Silvestre <francisco.silvestre@escolar.ifrn.edu.br>
2026-09-18 14:24:57 -0300

    atualizão no index e criação dos apps

---
+++ script/app1.js
@@ -0,0 +1,7 @@
+const amigos = ["Pedro", "Ana", "Clara"];
+tam = amigos.length
+
+console.log(`Arranjo ${amigos}`);
+onsole.log(`Tamanho ${tam}`);
+console.log(`Pimeiro ${amigos[0]}`);
+console.log(`Último ${amigos[tam-1]}`);
